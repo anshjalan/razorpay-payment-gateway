2ed5efdda7e514ffd098e6b21961c62db7e03e5e
anshjalan <jalanansh25@gmail.com>
2025-06-23 03:40:19 +0530

    updated var

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ import payment from './routes/payment.js'
 
 connectToMongo();
 const app = express()
-const port = 4000
-
+const port = process.env.PORT
 // middleware
 app.use(express.json());
 app.use(cors());
